1added25c79649f042b1ce5edbac76af188cef83
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-11-05 16:05:50 -0500

    API key added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,16 +3,32 @@
       var animals = ["Cat", "Dog", "Zebra", "Lion"];
 
       
-      function alertMovieName() {
-        var animalName = $(this).attr("data-name");
-        alert(animalName); 
- 
-      }
+      
+// function for getting gifs of animals
+addAnimalGif = () => {
+    var animalName = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/" + animalName +"?api_key=Xh77ZakqrTb0oj9MNEtgN8pyWM7hqHJo"
 
-      // Function for rendering buttons to page
+    $.ajax({
+        url:queryURL, 
+        method:"GET",
+    }).then(function(response){
+        var animalDiv = $("<div class = 'animalGif'"); 
+        var rating = response.Rating;
+        var ratingDisplay = $("<p>").text("Rating: "+ rating); 
+        animalDiv.append(ratingDisplay); 
+        var imgURL = respose.Poster; 
+        var image = $("<img>").attr("src",imgURL);
+        animalDiv.append(image); 
+        $("animalContent").prepend(animalDiv); 
+        
+
+    })
+}
+       // Function for rendering buttons to page
       function createButtons() {
 
-        // $("#buttonsAdded").empty();
+        $("#buttonsAdded").empty();
         for (var i = 0; i < animals.length; i++) {
 
           // dynamicaly generating buttons for each movie in the array
@@ -23,17 +39,19 @@
           $("#buttonsAdded").append(a);
         }
       }
-
+// add on click event to push new animals to array
       $("#add-animal").on("click", function(event) {
         event.preventDefault();
     
-        var animal = $("#animal-input").val().trim();
+        var inputAnimal = $("#animal-input").val().trim();
 
 
-        animals.push(animal);
+        animals.push(inputAnimal);
 
         
         createButtons();
 
       });
+
+    
       
