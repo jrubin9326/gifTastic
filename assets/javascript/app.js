7df53bb215ca5667c229f8e0371984bdd356a81b
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-11-05 22:46:37 -0500

    working API link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,31 @@
 
       // Initial array of animals
       var animals = ["Cat", "Dog", "Zebra", "Lion"];
-
-      
+      const APIkey = "Xh77ZakqrTb0oj9MNEtgN8pyWM7hqHJo"
+      var limit = 10
       
 // function for getting gifs of animals
-addAnimalGif = () => {
+$(document).on("click", function() {
     var animalName = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/" + animalName +"?api_key=Xh77ZakqrTb0oj9MNEtgN8pyWM7hqHJo"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIkey + "&q="+animalName +"&limit=10&rating=pg"
 
     $.ajax({
         url:queryURL, 
         method:"GET",
     }).then(function(response){
-        var animalDiv = $("<div class = 'animalGif'"); 
-        var rating = response.Rating;
-        var ratingDisplay = $("<p>").text("Rating: "+ rating); 
-        animalDiv.append(ratingDisplay); 
-        var imgURL = respose.Poster; 
-        var image = $("<img>").attr("src",imgURL);
-        animalDiv.append(image); 
-        $("animalContent").prepend(animalDiv); 
-        
+        console.log(response)
+        // var animalDiv = $("<div class = 'animalGif'"); 
+        // var rating = response.Rating;
+        // var ratingDisplay = $("<p>").text("Rating: "+ rating); 
+        // animalDiv.append(ratingDisplay); 
+        // var imgURL = respose.Poster; 
+        // var image = $("<img>").attr("src",imgURL);
+        // animalDiv.append(image); 
+        // $("animalContent").prepend(animalDiv); 
+
 
     })
-}
+})
        // Function for rendering buttons to page
       function createButtons() {
 
