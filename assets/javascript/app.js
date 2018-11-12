c10aa10ae3e45e070d3050e85763ed162b38e1c4
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-11-12 17:43:34 -0500

    logic updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,10 @@
       var limit = 10
       
 // function for getting gifs of animals
-$("#buttonsAdded").on("click", function() {
-    var animalName = "zebra"
+$("#buttonsAdded").on("click", ".animal",function() {
+    var animalName = $(this).attr("data-name")
+    
+    console.log(animalName); 
     console.log(animalName)
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIkey + "&q="+ animalName +"&limit=" + limit + "&rating=pg"
@@ -23,14 +25,31 @@ $("#buttonsAdded").on("click", function() {
         console.log(response)
         var ratingDisplay = $("<p>").text("Rating: "+ rating); 
         animalDiv.append(ratingDisplay); 
-        var imgURL = response.data[i].images.fixed_height_still.url
-        var image = $("<img>").attr("src",imgURL);
+        var imageURL = response.data[i].images.fixed_height_still.url
+        var image = $("<img class = 'state'>")
+        image.attr("src", imageURL)
+        image.attr("data-still", imageURL); 
+        image.attr("data-animate", response.data[i].images.original)
+        image.attr("data-state", "still")
         animalDiv.append(image); 
         $("#animalContent").append(animalDiv); 
         }
 
     })
 })
+
+$(document).on("click", ".state", function(){
+    var imgState = $(this).attr("data-state");
+    
+    if (state === "still") {
+        $(this).attr("data-state", "animated")} 
+        else {
+            $(this).attr("data-state", "still");
+            $(this).attr("src", $(this).attr("data-still"));
+        }
+    });
+     
+
 
 
        // Function for rendering buttons to page
@@ -42,8 +61,9 @@ $("#buttonsAdded").on("click", function() {
           // dynamicaly generating buttons for each movie in the array
           var a = $("<button>");
           a.addClass("animal");
-          a.attr("data-name",animals[i]);
+          a.attr("data-name", animals[i]);
           a.text(animals[i]);
+          console.log(a)
           $("#buttonsAdded").append(a);
         }
       }
